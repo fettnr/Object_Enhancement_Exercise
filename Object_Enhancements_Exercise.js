c1b61ef48f225d1486dc647373208dea769b19f6
fettnr316 <fettnr316@gmail.com>
2022-12-08 17:57:03 -0500

    Completed Exercise

---
+++ Object_Enhancements_Exercise.js
@@ -0,0 +1,42 @@
+// Same keys and values ES2015
+function createInstructor(firstName, lastName){
+    return {
+      firstName,
+      lastName
+    }
+  }
+
+// Computed Property Names ES2015
+let favoriteNumber = 42;
+
+const instructor = {
+  firstName: "Colt",
+  [favoriteNumber]: "That is my favorite!"
+}
+
+// Object Methods ES2015
+const instructorObj = {
+    firstName: "Colt",
+    sayHi(){
+      return "Hi!";
+    },
+    sayBye(){
+      return this.firstName + " says bye!";
+    }
+  }
+
+// createAnimal function
+  const d = createAnimal("dog", "bark", "Woooof!")
+  d.bark()
+  
+  const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+  s.bleet()
+  
+  function createAnimal(species, verb, noise){
+    return {
+      species,
+      [verb](){
+        return noise;
+      }
+    }
+  }
